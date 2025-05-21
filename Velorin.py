e4a7f3df38f22ab6c3258d4f5b64ce994e714b60
minedefer314 <azebuty@gmail.com>
2025-05-21 17:58:27 +0200

    fix pyautogui.screenshot region parameter

---
+++ Velorin.py
@@ -0,0 +1,64 @@
+import os
+import pyautogui
+import time
+import keyboard
+import psutil
+import win32gui
+import numpy as np
+
+
+
+def get_active_window_size():
+    hwnd = win32gui.GetForegroundWindow()
+    if hwnd == 0:
+        print('No active window detected.')
+        return (None, None)
+
+    rect = win32gui.GetWindowRect(hwnd)
+    width = rect[2] - rect[0]
+    height = rect[3] - rect[1]
+    return (width, height)
+
+def pixel_search(color, region, tolerance=20, stride=3):
+    screenshot = pyautogui.screenshot(region=region)
+    img_array = np.array(screenshot)
+    for y in range(0, img_array.shape[0], stride):
+        for x in range(0, img_array.shape[1], stride):
+            pixel = img_array[y, x, :3]
+            if np.all(np.abs(pixel - color) <= tolerance):
+                return True
+    return False
+
+def get_mouse_color():
+    x, y = pyautogui.position()
+    screenshot = pyautogui.screenshot()
+    return screenshot.getpixel((x, y))
+
+def terminate_process():
+    pid = os.getpid()
+    parent = psutil.Process(pid)
+    for child in parent.children(recursive=True):
+        child.terminate()
+    parent.terminate()
+    os._exit(0)
+
+def main():
+    color_to_find = None
+    region = None
+    while True:
+        if keyboard.is_pressed('ctrl'):
+            color_to_find = get_mouse_color()
+            win_width, win_height = get_active_window_size()
+            mid_x = win_width // 2
+            mid_y = win_height // 2
+            region = (mid_x-5, mid_y-5, 10, 10)
+
+        elif keyboard.is_pressed('e') and region is not None and pixel_search(color_to_find, region):
+            pyautogui.click()
+            time.sleep(0.4)
+        elif keyboard.is_pressed('f2'):
+            terminate_process()
+        else:
+            time.sleep(0.01)
+if __name__ == '__main__':
+    main()
